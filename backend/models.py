df0f5704b202e8e3ceb46d56663643f166d166d7
evgeny_jt <dylan_jt@mail.ru>
2025-03-15 17:18:37 +0300

    Initial commit

---
+++ backend/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    email = models.EmailField(unique=True)
+
+
+class Shop(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    # url = models.URLField()
+
+# class Category(models.Model):
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#
+# class Product(models.Model):
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#
+# class ProductInfo(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     quantity = models.PositiveIntegerField()
+#     price = models.ImageField()
+#     price_rrc = models.ImageField()
+#
+# class Parametr(models.Model):
+#     name = models.CharField(max_length=100)
+#
+# class ProductParametr(models.Model):
+#     product_info = models.ForeignKey(ProductInfo, on_delete=models.CASCADE)
+#     parametr = models.ForeignKey(Parametr, on_delete=models.CASCADE)
+#     value = models.CharField(max_length=100)
+#
+# class Order(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     dt = models.DateTimeField(auto_now_add=True)
+#     status = models.CharField(max_length=10)
+#
+# class OrderItem(models.Model):
+#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField()
+#
+# class Contact(models.Model):
+#     type = models.CharField(max_length=100)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     value = models.CharField(max_length=100)
---
+++ backend/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from orders.serializers import ShopSerializer
+from .models import Shop
+
+
+# @api_view(['GET'])
+# def test(request):
+#     print('запрос в базу')
+#     shop = Shop.objects.all()
+#     ser = ShopSerializer(shop, many=True)
+#     print('shop - ', shop)
+#     data = {'jt': 'jt_test1'}
+#     return Response(ser.data)
+
+
+# class OrderView(APIView):
+#     def get(self, request):
+#         shop = Shop.objects.all()
+#         ser = ShopSerializer(shop, many=True)
+#         return Response(ser.data)
+#
+#     def post(self, request):
+#         return Response({'status': 'ok'})
+
+class OrderView(ListAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+    def post(self, request):
+        return Response({'status': 'ok'})
